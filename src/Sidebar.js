b0c255e832e2cb2bd95e50f3d47fafc57baaf488
Rayyan Karim <rkarim0598@gmail.com>
2018-05-22 15:27:56 -0400

    Add some sidebar styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,9 +6,18 @@ import newHover from './new-hover.png'
 
 const Sidebar = () => {
     return (
-        <nav class="Sidebar">
-          <div class="logo">
-            <img src={quill} alt="Noteherder" />
+        <nav 
+            class="Sidebar"
+            style={styles.sidebar}
+        >
+          <div 
+            class="logo"
+            style={styles.logo}
+          >
+            <img 
+                src={quill} alt="Noteherder"
+                style={styles.logoImg} 
+            />
           </div>
           <a class="new-note" href="/notes">
             <img src={newHover} alt="New note" />
@@ -23,4 +32,23 @@ const Sidebar = () => {
     )
 }
 
+const styles = {
+    sidebar: {
+        width: '6rem',
+        backgroundColor: '#f3f3f3',
+        padding: '0.5rem 0',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    logo: {
+        fontFamily: '"Fauna One"',
+        color: '#666',
+        fontSize: '3rem',
+    },
+    logoImg: {
+        width: '3rem',
+        paddingLeft: '0.4rem',
+    }
+}
 export default Sidebar
