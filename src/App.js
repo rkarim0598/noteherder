fb2453dda31c3f6b02430948209848e9d9d05526
Rayyan Karim <rkarim0598@gmail.com>
2018-05-23 00:48:40 -0400

    Add more formatting and render list using state and map

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import './App.css';
 import Main from './Main'
 
 class App extends Component {
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     titles: ['Kohlrabi welsh', 'Dandelion cucumber', 'Prairie turnip' ],
+  //     notes: ['Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.',
+  //             'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.',
+  //             'Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.'
+  //            ]
+  //   }
+  // }
   render() {
     return (
       <div className="App">
--- src/NoteList.js
+++ src/NoteList.js
@@ -1,55 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const NoteList = () => {
-    return (
-        <div className="NoteList" style={styles.noteList}>
-            <h3 style={styles.h3}>Notes</h3>
-            <ul id="notes" style={styles.ulNotes}>
-                <a className="active" style={styles.ulA}>
-                    <li style={styles.ulLi}>
-                        <div className="note">
-                            <div className="note-title">
-                                Kohlrabi welsh
+class NoteList extends Component {
+    constructor() {
+        super()
+        this.state = {
+            titles: ['Kohlrabi welsh', 'Dandelion cucumber', 'Prairie turnip'],
+            notes: ['Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.',
+                'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.',
+                'Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.'
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className="NoteList" style={styles.noteList}>
+                <h3 style={styles.h3}>Notes</h3>
+                <ul id="notes" style={styles.ulNotes}>
+                    <a className="active" style={styles.ulA}>
+                        <li style={styles.ulLi}>
+                            <div class="note" style={styles.note}>
+                                <div class="note-title" style={styles.noteTitle}>
+                                    {this.state.titles[0]}
+                                </div>
+                                <div class="note-body" style={styles.noteBody}>
+                                    <p>
+                                        {this.state.notes[0]}
+                                    </p>
+                                </div>
                             </div>
-                            <div className="note-body">
-                                <p>
-                                    Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                </a>
-                <a style={styles.ulA}>
-                    <li style={styles.ulLi}>
-                        <div className="note">
-                            <div className="note-title">
-                                Dandelion cucumber
-                            </div>
-                            <div className="note-body">
-                                <p>
-                                    Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                </a>
-                <a style={styles.ulA}>
-                    <li style={styles.ulLi}>
-                        <div className="note">
-                            <div className="note-title">
-                                Prairie turnip
-                            </div>
-                            <div className="note-body">
-                                <p>
-                                    Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                </a>
-            </ul>
-        </div>
-    )
+                        </li>
+                    </a>
+                    <a style={styles.ulA}>
+                        {this.state.titles.slice(1).map(title =>
+                            <li style={styles.ulLi}>
+                                <div className="note" style={styles.note}>
+                                    <div className="note-title" style={styles.noteTitle}>
+                                        {title}
+                                    </div>
+                                    <div className="note-body" style={styles.noteBody}>
+                                        <p>
+                                            {this.state.notes[this.state.titles.indexOf(title)]}
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                        )}
+                    </a>
+                </ul>
+            </div>
+        )
+    }
 }
 
 const styles = {
