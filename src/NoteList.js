1cb953b9028ddbef537ece7c96fef1a2ff2b0dac
Rayyan Karim <rkarim0598@gmail.com>
2018-05-22 22:07:55 -0400

    Add all sidebar formatting and some notelist formatting

--- src/NoteList.js
+++ src/NoteList.js
@@ -2,10 +2,91 @@ import React from 'react'
 
 const NoteList = () => {
     return (
-        <div className="NoteList">
-            NoteList
+        <div class="NoteList" style={styles.noteList}>
+            <h3 style={styles.h3}>Notes</h3>
+            <ul id="notes">
+                <a class="active">
+                    <li>
+                        <div class="note">
+                            <div class="note-title">
+                                Kohlrabi welsh
+                            </div>
+                            <div class="note-body">
+                                <p>
+                                    Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                </a>
+                <a>
+                    <li>
+                        <div class="note">
+                            <div class="note-title">
+                                Dandelion cucumber
+                  </div>
+                            <div class="note-body">
+                                <p>
+                                    Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                </a>
+                <a>
+                    <li>
+                        <div class="note">
+                            <div class="note-title">
+                                Prairie turnip
+                  </div>
+                            <div class="note-body">
+                                <p>
+                                    Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
+                    </p>
+                            </div>
+                        </div>
+                    </li>
+                </a>
+            </ul>
         </div>
     )
+}
+
+const styles = {
+    noteList: {
+        borderLeft: '1px solid #eee',
+        borderRight: '1px solid #eee',
+        width: '30rem'
+    },
+    h3: {
+        color: '#999',
+        textTransform: 'uppercase',
+        fontSize: '2rem',
+        fontFamily: 'Oxygen',
+        fontWeight: '300',
+        letterSpacing: '3px',
+        margin: '20px 2rem'
+    },
+    ulNotes: {
+        borderTop: '1px solid #eee',
+        overflowY: 'scroll',
+        height: 'calc(100vh - 72px)',
+        listStyle: 'none',
+        marginTop: '1em',
+        padding: '0',
+        width: '100%',
+        color: '#999'
+    },
+    ulA: {
+        color: 'inherit',
+        textDecoration: 'none'
+    },
+    ulLi: {
+        height: '100px',
+        fontSize: '90%',
+        cursor: 'pointer',
+        overflow: 'hidden'
+    },
 }
 
 export default NoteList
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,30 +6,55 @@ import newHover from './new-hover.png'
 
 const Sidebar = () => {
     return (
-        <nav 
-            class="Sidebar"
+
+        <nav
+            className="Sidebar"
             style={styles.sidebar}
         >
-          <div 
-            class="logo"
-            style={styles.logo}
-          >
-            <img 
-                src={quill} alt="Noteherder"
-                style={styles.logoImg} 
-            />
-          </div>
-          <a class="new-note" href="/notes">
-            <img src={newHover} alt="New note" />
-            <img class="outline" src={newIcon} alt="New note" />
-          </a>
-          <div class="SignOut">
-            <button>
-              <i class="fa fa-sign-out"></i>
-            </button>
-          </div>
-        </nav>    
+            
+            <div
+                className="logo"
+                style={styles.logo}
+            >
+                <img
+                    src={quill} alt="Noteherder"
+                    style={styles.logoImg}
+                />
+            </div>
+            <a className="new-note" href="/notes" style={styles.newNote} 
+                onMouseEnter={invisible} onMouseLeave={visible}>
+                <img
+                    name="initPhoto"
+                    src={newHover} alt="New note"
+                    style={styles.aImg}                    
+                />
+                <img
+                    name="secondPhoto"
+                    className="outline" src={newIcon} alt="New note"
+                    style={styles.aImg}
+                    
+                />
+            </a>
+            <div
+                className="SignOut"
+                style={styles.signOut}
+            >
+                <button style={styles.button}>
+                    <i className="fa fa-sign-out" style={styles.iFa}></i>
+                </button>
+            </div>
+           
+        </nav>
     )
+}
+
+function invisible() {
+    document.querySelector('.new-note').children[1].style.opacity = 0
+
+}
+
+function visible() {
+    document.querySelector('.new-note').children[1].style.opacity = 1
 }
 
 const styles = {
@@ -49,6 +74,34 @@ const styles = {
     logoImg: {
         width: '3rem',
         paddingLeft: '0.4rem',
+    },
+    newNote: {
+        marginTop: '2rem',
+        position: 'relative',
+        width: '40px'
+    },
+    button: {
+        backgroundColor: 'transparent',
+        border: '0',
+        color: '#008BF8',
+        cursor: 'pointer'
+    },
+    aImg: {
+        position: 'absolute',
+        left: '0',
+        width: '100%',
+        transition: 'opacity 0.25s ease-in-out'
+    },
+    signOut: {
+        position: 'absolute',
+        bottom: '1rem',
+    },
+    iFa: {
+        fontSize: '2rem'
+    },
+    hover: {
+        ...this.aImg,
+        opacity: '0'
     }
 }
 export default Sidebar
