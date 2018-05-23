63452d41bb1c82f23db457a48b562b1987b31238
Rayyan Karim <rkarim0598@gmail.com>
2018-05-23 15:12:22 -0400

    Set current note upon clicking one from list

--- src/Main.js
+++ src/Main.js
@@ -8,15 +8,26 @@ class Main extends Component {
     constructor() {
         super()
         this.state = {
-            titles: ['Kohlrabi welsh', 'Dandelion cucumber', 'Prairie turnip'],
-            notes: ['Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.',
-                'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.',
-                'Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.'
+            currentNote: null,
+            notes: [
+                {
+                    id: 1,
+                    title: 'JS thoughts',
+                    body: 'I just love JS so much.',
+                },
+                {
+                    id: 2,
+                    title: 'Breakfast',
+                    body: 'FOR IT!',
+                },
             ],
-            titulo: '',
-            nota: ''
         }
     }
+
+    setCurrentNote = (note) => {
+        this.setState({currentNote: note})
+    }
+
     render() {
         return (
             <div
@@ -24,24 +35,19 @@ class Main extends Component {
                 style={style}
             >
                 <Sidebar />
-                <NoteList texty={this.state}
-                    onClick={clickSomething.bind(this)}
+                <NoteList notes={this.state.notes}
+                          setCurrentNote={this.setCurrentNote}
                 />
-                <NoteForm texty={this.state}/>
+                <NoteForm />
             </div>
         )
     }
 }
-function clickSomething(ev) {
-    const title = ev.target.closest('li').querySelector('.note-title').textContent
-    const bowdy = ev.target.closest('li').querySelector('.note-body').textContent
-    
-    this.setState({ titulo: title, nota: bowdy})
-}
+
 const style = {
     display: 'flex',
     height: '100vh',
-    alignItems: 'stretch'
+    alignItems: 'stretch',
 }
 
 export default Main
--- src/NoteList.js
+++ src/NoteList.js
@@ -3,29 +3,15 @@ import React from 'react'
 import './NoteList.css'
 import Note from './Note'
 
-const NoteList = () => {
-    const notes = [
-        {
-            id: 1,
-            title: 'JS thoughts',
-            body: 'blah blah blah',
-        },
-        {
-            id: 2,
-            title: 'Breakfast',
-            body: 'nope'
-        },
-        {
-            id: 3,
-            title: 'Class notes',
-            body: 'React me act'
-        }
-    ]
+const NoteList = (props) => {
+    const notes = props.notes
   return (
     <div className="NoteList">
       <h3>Notes</h3>
       <ul id="notes">
-        { notes.map(note => <Note key={note.id} note={note}/> ) }        
+        { notes.map(note => <Note key={note.id} note={note} 
+            setCurrentNote={props.setCurrentNote}
+        /> ) }        
       </ul>
     </div>
   )
