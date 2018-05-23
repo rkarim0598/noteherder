a6f9e0939b7abe4c021cc5b0543d192daad51e76
Rayyan Karim <rkarim0598@gmail.com>
2018-05-23 14:37:48 -0400

    Add key prop to each note

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,80 +1,28 @@
 import React from 'react'
 
-class NoteForm extends React.Component {
-    render() {
-        return (
-            <div className="NoteForm" style={styles.noteForm}>
-                <div className="form-actions" style={styles.formActions}>
-                    <button type="button" style={styles.button}>
-                        <i className="fa fa-trash-o" style={styles.buttonIcon}></i>
-                    </button>
-                </div>
-                <form style={styles.form}>
-                    <p>
-                        <input
-                            type="text"
-                            name="title"
-                            placeholder="Title your note"
-                            style={styles.inputTitle}
-                            value={this.props.texty.titulo}
-                        />
-                    </p>
+import './NoteForm.css'
 
-                    <textarea name="body" style={styles.textArea} value={this.props.texty.nota} ></textarea>
-          </form>
-            </div>
-        )
-    }
+const NoteForm = () => {
+  return (
+    <div className="NoteForm">
+      <div className="form-actions">
+        <button type="button">
+          <i className="far fa-trash-alt"></i>
+        </button>
+      </div>
+      <form>
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title your note"
+          />
+        </p>
+
+        <textarea name="body"></textarea>
+      </form>
+    </div>
+  )
 }
 
-const styles = {
-    noteForm: {
-        // msFlexPositive: '1',
-        flexGrow: '1',
-        padding: '0 3rem'
-    },
-    formActions: {
-        paddingTop: '1rem',
-        marginLeft: '-2rem',
-        display: 'flex',
-        // mxFlexLinePack: 'center',
-        alignContent: 'center'
-    },
-    button: {
-        border: 'none',
-        background: 'none',
-        padding: '0'
-    },
-    buttonSubmit: {
-        backgroundColor: '#008bf8',
-        borderRadius: '4px',
-        color: '#fff',
-        fontSize: '1.4rem',
-        padding: '1rem'
-    },
-    buttonIcon: {
-        color: '#999',
-        fontSize: '2rem',
-        margin: '0'
-    },
-    form: {
-        margin: '0 auto',
-        maxWidth: '80rem'
-    },
-    inputTitle: {
-        border: 'none',
-        fontSize: '200%',
-        fontFamily: '"Fauna One"',
-        color: '#008bf8',
-        fontWeight: '400',
-        width: '100%',
-        outline: 'none'
-    },
-    textArea: {
-        borderColor: '#a676f7',
-        width: '100%',
-        height: 'calc(100vh - 140px)',
-        fontSize: '1.3em'
-    }
-}
 export default NoteForm
--- src/NoteList.js
+++ src/NoteList.js
@@ -1,131 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class NoteList extends Component {
-    // constructor() {
-    //     super()
-    //     this.state = {
-    //         titles: ['Kohlrabi welsh', 'Dandelion cucumber', 'Prairie turnip'],
-    //         notes: ['Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.',
-    //             'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.',
-    //             'Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.'
-    //         ]
-    //     }
-    // }
+import './NoteList.css'
+import Note from './Note'
 
-    render() {
-        return (
-            <div className="NoteList" style={styles.noteList}>
-                <h3 style={styles.h3}>Notes</h3>
-                <ul id="notes" style={styles.ulNotes}>
-                    <a className="active" style={styles.ulA} >
-                        <li style={styles.ulLi} onClick={this.props.onClick}>
-                            <div className="note" style={styles.note}>
-                                <div className="note-title" style={styles.noteTitle}>
-                                    {this.props.texty.titles[0]}
-                                </div>
-                                <div className="note-body" style={styles.noteBody}>
-                                    <p>
-                                        {this.props.texty.notes[0]}
-                                    </p>
-                                </div>
-                            </div>
-                        </li>
-                    </a>
-                    {this.props.texty.titles.slice(1).map(title =>
-                        <a style={styles.ulA} >
-                            <li style={styles.ulLi} onClick={this.props.onClick}>
-                                <div className="note" style={styles.note}>
-                                    <div className="note-title" style={styles.noteTitle}>
-                                        {title}
-                                    </div>
-                                    <div className="note-body" style={styles.noteBody}>
-                                        <p>
-                                            {this.props.texty.notes[this.props.texty.titles.indexOf(title)]}
-                                        </p>
-                                    </div>
-                                </div>
-                            </li>
-                        </a>
-                    )}
-                </ul>
-            </div>
-        )
-    }
-}
-
-
-const styles = {
-    noteList: {
-        borderLeft: '1px solid #eee',
-        borderRight: '1px solid #eee',
-        width: '30rem'
-    },
-    h3: {
-        color: '#999',
-        textTransform: 'uppercase',
-        fontSize: '2rem',
-        fontFamily: 'Oxygen',
-        fontWeight: '300',
-        letterSpacing: '3px',
-        margin: '20px 2rem'
-    },
-    ulNotes: {
-        borderTop: '1px solid #eee',
-        overflowY: 'scroll',
-        height: 'calc(100vh - 72px)',
-        listStyle: 'none',
-        marginTop: '1em',
-        padding: '0',
-        width: '100%',
-        color: '#999'
-    },
-    ulA: {
-        color: 'inherit',
-        textDecoration: 'none'
-    },
-    ulLi: {
-        height: '100px',
-        fontSize: '90%',
-        cursor: 'pointer',
-        overflow: 'hidden'
-    },
-    ulNotesA: {
-        WebkitTransition: 'background-color .1s ease-in-out, color .1s ease-in-out',
-        OTransition: 'background-color .1s ease-in-out, color .1s',
-        transition: 'background-color .1s ease-in-out, color .1s ease-in-out'
-    },
-    liNote: {
-        borderTop: '1px solid #eee',
-        margin: '0 2rem',
-        padding: '1rem 4px'
-    },
-    aLiNote: {
-        borderTop: 'none'
-    },
-    noteTitle: {
-        color: '#4a4a4a',
-        fontFamily: '"Fauna One"',
-        fontSize: '120%',
-        fontWeight: '400',
-        whiteSpace: 'nowrap',
-        overflowX: 'hidden',
-        OTextOverflow: 'ellipsis',
-        textOverflow: 'ellipsis'
-    },
-    noteBody: {
-        fontSize: '1m!important',
-        margin: '0!important',
-        padding: '0!important',
-        color: '#999!important',
-        background: 'none',
-        border: 'none'
-    },
-    note: {
-        display: 'block',
-        height: '100px',
-        padding: '1em',
-        textDecoration: 'none'
-    }
+const NoteList = () => {
+    const notes = [
+        {
+            id: 1,
+            title: 'JS thoughts',
+            body: 'blah blah blah',
+        },
+        {
+            id: 2,
+            title: 'Breakfast',
+            body: 'nope'
+        },
+        {
+            id: 3,
+            title: 'Class notes',
+            body: 'React me act'
+        }
+    ]
+  return (
+    <div className="NoteList">
+      <h3>Notes</h3>
+      <ul id="notes">
+        { notes.map(note => <Note key={note.id} note={note}/> ) }        
+      </ul>
+    </div>
+  )
 }
 
 export default NoteList
