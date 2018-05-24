3c63e12d0569c7905c1ffdb112144bdbcec3a14a
Rayyan Karim <rkarim0598@gmail.com>
2018-05-24 14:46:37 -0400

    Change vars to local scope

--- src/rebase.js
+++ src/rebase.js
@@ -2,7 +2,7 @@ import Rebase from 're-base'
 import firebase from 'firebase/app'
 import 'firebase/database'
 
-var app = firebase.initializeApp({
+const app = firebase.initializeApp({
     apiKey: "AIzaSyAh-rT7wUtOOUUbw6fozxj7bZdRCy4TmPg",
     authDomain: "note-keeper-b8e53.firebaseapp.com",
     databaseURL: "https://note-keeper-b8e53.firebaseio.com",
@@ -12,7 +12,7 @@ var app = firebase.initializeApp({
   })
 //   firebase.initializeApp(config);
 
-var db = firebase.database(app)
-var base = Rebase.createClass(db)
+const db = firebase.database(app)
+const base = Rebase.createClass(db)
 
 export default base
