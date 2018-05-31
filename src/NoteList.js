369cbb342a220c4da7f598228b4d27cf89a588de
Rayyan Karim <rkarim0598@gmail.com>
2018-05-30 23:22:29 -0400

    Fully update to Davey's code

--- src/NoteList.js
+++ src/NoteList.js
@@ -3,14 +3,18 @@ import React from 'react'
 import './NoteList.css'
 import Note from './Note'
 
-const NoteList = ( { notes, setCurrentNote, deleteNote } ) => {
+const NoteList = ({ notes }) => {
+
   return (
     <div className="NoteList">
       <h3>Notes</h3>
       <ul id="notes">
-        { notes.map(note => <Note key={note.id} note={note} 
-            setCurrentNote={setCurrentNote}
-        /> ) }        
+        { notes.map(note => (
+          <Note
+            key={note.id}
+            note={note}
+          />
+        ))}
       </ul>
     </div>
   )
